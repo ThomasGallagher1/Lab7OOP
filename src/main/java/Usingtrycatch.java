3d18679d7a0e348d4b756c602583964bf52b62bc
G00415457@atu.ie <g00415457@atu.ie>
2023-11-13 16:27:18 +0000

    Committed local changes

---
+++ src/main/java/Usingtrycatch.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Usingtrycatch {
+    public static void main(String[] args)
+    {
+        System.out.println("Please enter a number between 0 - 9 :");
+        Scanner myscan = new Scanner
+    }
+
+}
