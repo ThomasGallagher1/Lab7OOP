d461e4eb115088de2a1053b1fc732f8ca5c8afc4
G00415457@atu.ie <g00415457@atu.ie>
2023-11-13 16:58:29 +0000

    Added while(true) and also a try catch

--- src/main/java/Usingtrycatch.java
+++ src/main/java/Usingtrycatch.java
@@ -1,9 +1,22 @@
 import java.util.Scanner;
-public class Usingtrycatch {
-    public static void main(String[] args)
-    {
-        System.out.println("Please enter a number between 0 - 9 :");
-        Scanner myscan = new Scanner
-    }
 
+public class Usingtrycatch {
+    public static void main(String[] args) {
+        Scanner myscan = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("Please enter a number between 0 - 9 :");
+                String input = myscan.nextLine();
+                int myNum = Integer.parseInt(input);
+                if (myNum >= 0 && myNum <= 9) {
+                    System.out.println("You entered " + myNum);
+                    break;
+                } else {
+                    System.out.println("Invalid number");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input");
+            }
+        }
+    }
 }
