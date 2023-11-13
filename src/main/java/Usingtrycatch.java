94ce17ded0a58f362b75739f47ddd55c8c08fee2
G00415457@atu.ie <g00415457@atu.ie>
2023-11-13 17:03:26 +0000

    added feature 2

--- src/main/java/Usingtrycatch.java
+++ src/main/java/Usingtrycatch.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 public class Usingtrycatch {
     public static void main(String[] args)
     {
+        while(true);
+        try{
+
+        }
         System.out.println("Please enter a number between 0 - 9 :");
-        Scanner myscan = new Scanner
+        Scanner myscan = new Scanner(System.in);
+        String input = myscan
     }
 
 }
